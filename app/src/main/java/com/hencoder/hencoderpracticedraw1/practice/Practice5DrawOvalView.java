37239ffe812b00f791df7af26021c11c062f63cb
Chenstyle <lizhizhao81@gmail.com>
2020-01-22 10:46:18 +0800

    draw oval

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice5DrawOvalView extends View {
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public Practice5DrawOvalView(Context context) {
         super(context);
@@ -25,5 +30,18 @@ public class Practice5DrawOvalView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
+        // Paint 默认颜色就是黑色，方式是填充
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        int left = getWidth() / 4;
+        int top = getHeight() / 4;
+        int right = left * 3;
+        int bottom = top * 3;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            canvas.drawOval(left, top, right, bottom, mPaint);
+        } else {
+            // 不要在onDraw里面新建对象
+            canvas.drawOval(new RectF(left, top, right, bottom), mPaint);
+        }
     }
 }
