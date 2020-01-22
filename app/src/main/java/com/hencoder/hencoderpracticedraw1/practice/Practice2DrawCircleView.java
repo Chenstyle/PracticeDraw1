6ba527903ce5ef5efb2639fec58f0cc485056a97
Chenstyle <lizhizhao81@gmail.com>
2020-01-22 10:26:25 +0800

    drawable color circle

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,11 +2,15 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+
+    private Paint mPaint = new Paint();
 
     public Practice2DrawCircleView(Context context) {
         super(context);
@@ -26,5 +30,27 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        mPaint.setColor(Color.BLACK);
+        // 实心圆
+        mPaint.setStyle(Paint.Style.FILL);
+        int circle1X = getWidth() / 4;
+        int circle1Y = getHeight() / 4;
+        int circleRadius = getWidth() / 8;
+        canvas.drawCircle(circle1X, circle1Y, circleRadius, mPaint);
+        // 空心圆
+        mPaint.setStyle(Paint.Style.STROKE);
+        int circle2X = circle1X * 3;
+        canvas.drawCircle(circle2X, circle1Y, circleRadius, mPaint);
+        // 蓝色实心圆
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLUE);
+        int circle3Y = circle1Y * 3;
+        canvas.drawCircle(circle1X, circle3Y, circleRadius, mPaint);
+        // 线宽为20的空心圆
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(20);
+        mPaint.setColor(Color.BLACK);
+        int circle4X = circle1X *3;
+        canvas.drawCircle(circle4X, circle3Y, circleRadius, mPaint);
     }
 }
